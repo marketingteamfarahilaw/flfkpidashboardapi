0cb18d25ede59c7b291cd6a695af579ca2bf0420
Raimehn Roger <raimehnroger@gmail.com>
2025-10-01 12:25:38 -0700

    remove openAI library

--- application/controllers/Kpi.php
+++ application/controllers/Kpi.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 
-require_once APPPATH . 'libraries/OpenAIWrapper.php';
+// require_once APPPATH . 'libraries/OpenAIWrapper.php';
 class Kpi extends REST_Controller {
     function __construct() {
         parent::__construct();
