a3b31b2ce1958649797cc723e33fe2b01bb46043
Raimehn Roger <raimehnroger@gmail.com>
2025-10-08 15:59:09 -0700

    push test

--- application/controllers/Kpi.php
+++ application/controllers/Kpi.php
@@ -2486,6 +2486,7 @@ class Kpi extends REST_Controller {
     // content
     function add_content_post()
     {
+      die('yeah');
         // Grab payload (works for form-data or JSON)
         $raw = $this->input->raw_input_stream;
         if (!empty($raw) && empty($_POST)) {
